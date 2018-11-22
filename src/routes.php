b132e49328bfa5db08cd53ec06d90ff28c7be999
weiguo.ni <neefrankie@gmail.com>
2018-11-22 11:27:53 +0800

    give up php

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,28 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+$mw = function(Request $request, Response $response, $next) {
+    $request = $request->withAttribute("pricingPlans", [
+        "premium_year" => [
+            "tier" => "高端会员",
+            "cycle" => "年",
+            "price" => 1998.00,
+        ],
+        "standard_month" => [
+            "tier" => "标准会员",
+            "cycle" => "月",
+            "price" => 28.00,
+        ],
+        "standard_year" => [
+            "tier" => "标准会员",
+            "cycle" => "年",
+            "price" => 198.00,
+        ],
+    ]);
+
+    return $response;
+};
+
 // Routes
 $app->get('/', function (Request $request, Response $response, array $args) {
 
@@ -16,16 +38,30 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $response;
 });
 
-$app->get('/phpinfo', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write(phpinfo());
-    return $response;
-});
-
 $app->get('/subscription', function (Request $request, Response $response, array $args) {
-    $body = $this->renderer->render('/subscription.html');
+    $body = $this->renderer->render('subscription.html');
     $response->getBody()->write($body);
     return $response;
 });
+
+$app->get("/subscription/{tier}/{cycle}", function (Request $request, Response $response, array $args) {
+    $tier = $args["tier"];
+    $cycle = $args["cycle"];
+
+    $key = sprintf("%s_%s", $tier, $cycle);
+
+    // $pricingPlans = $request->getAttribute("pricingPlans");
+
+    // // if (!array_key_exists($key, $pricingPlans)) {
+    // //     return;
+    // // }
+
+    // $plan = $pricingPlans[$key];
+
+    $body = $this->renderer->render("payment.html");
+    // $response->getBody()->write(sprintf("Tier: %s. Billing Cycle: %s", $args["tier"], $args["cycle"]));
+    $response->getBody()->write($body);
+})->add($mw);
 
 
 
